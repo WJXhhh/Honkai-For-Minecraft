9bc59b592e98b4a9c7d0b42b9863d9c0c1f735fb
吴继信 <11297529+wjx4r@user.noreply.gitee.com>
2022-08-06 08:49:14 +0800

    v-32 添加了崩坏钢工具合成表

--- src/main/java/com/wjx/hkfm_mod/util/handlers/TileEntityHandler.java
+++ src/main/java/com/wjx/hkfm_mod/util/handlers/TileEntityHandler.java
@@ -6,5 +6,6 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class TileEntityHandler {
     public static void registerTileEntity(){
         GameRegistry.registerTileEntity(HK_TileEntities.TileEntity_hk_basic_grinder.class,"hk_basic_grinder");
+        GameRegistry.registerTileEntity(HK_TileEntities.TileEntity_mt_b_injector.class,"mt_b_injector");
     }
 }
